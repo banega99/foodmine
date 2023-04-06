7e45a688cbd7ce463429ee6b8e9afce1b686e201
Branislav Krompić <119957824+banega99@users.noreply.github.com>
2023-04-06 15:17:50 +0200

    gfgfgf

--- backend/built/server.js
+++ backend/built/server.js
@@ -2,9 +2,9 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
+dotenv_1["default"].config();
 var express_1 = __importDefault(require("express"));
 // import cors from "cors"
 // import { sample_foods, sample_tags, sample_users } from "./data"
@@ -17,15 +17,14 @@ var cors = require("cors");
 var corsOptions = {
     origin: '*',
     credentials: true,
-    optionSuccessStatus: 200,
-    //    origin:["http://localhost:5000"]
+    optionSuccessStatus: 200
 };
-var app = (0, express_1.default)();
-app.use(express_1.default.json());
+var app = (0, express_1["default"])();
+app.use(express_1["default"].json());
 app.use(cors(corsOptions));
-app.use("/api/foods", food_router_1.default);
-app.use("/api/users", user_router_1.default);
-app.use("/api/orders", order_router_1.default);
+app.use("/api/foods", food_router_1["default"]);
+app.use("/api/users", user_router_1["default"]);
+app.use("/api/orders", order_router_1["default"]);
 var port = 5000;
 app.listen(port, function () {
     console.log("Website served in http://localhost:" + port);
