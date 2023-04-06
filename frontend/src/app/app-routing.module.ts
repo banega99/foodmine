dd821f53e03200a9258e058ef1496a5d3e351bde
Branislav Krompić <119957824+banega99@users.noreply.github.com>
2023-04-06 12:12:59 +0200

    Nece biti poslednje koliko vidim

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { FoodPageComponent } from './components/pages/food-page/food-page.component';
 import { HomeComponent } from './components/pages/home/home.component';
-import { TestPageComponent } from './components/pages/test-page/test-page.component';
+// import { TestPageComponent } from './components/pages/test-page/test-page.component';
 import { LoginComponent } from './components/pages/login/login.component';
 import { RegisterComponent } from './components/pages/register/register.component';
 import { CheckoutPageComponent } from './components/pages/checkout-page/checkout-page.component';
@@ -17,7 +17,7 @@ const routes: Routes = [
   {path: 'food/:id', component:FoodPageComponent},
   {path: 'tag/:tag', component:HomeComponent},
   {path: 'cart-page', component:CartPageComponent},
-  {path: 'test-page', component:TestPageComponent},
+  // {path: 'test-page', component:TestPageComponent},
   {path: 'login', component:LoginComponent},
   {path: 'register', component:RegisterComponent},
   {path: 'checkout', component:CheckoutPageComponent, canActivate: [AuthGuard]},
